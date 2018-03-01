254318ee4f1c1d227b2e81950f8b74d0fcadf490
John Westberg <JWR@int.sonofon.dk>
2018-03-01 21:00:12 +0100

    adding vif analysis to mining functions

--- pjerrots_mining_functions.R
+++ pjerrots_mining_functions.R
@@ -12,9 +12,60 @@
 #starsplit : splits str into vector. Works with *
 #waffle_plot: created waffle plot (like pie chart - just better)
 #inf.omit: omits inf værdier (til modellering), så den kan bruges på samme måde som na.omit
+# vifanalysis: does VIF analysis and removes variables causing multicollinarity
 
 #############################################################
 # calculates gini and auc metrics from x,y value pairs. INclude ROC curve
+
+vifanalysis <- function(df,vars,vif_criteria=10) {
+
+  for (i in vars) {
+    if (is.character(df[,i])) df[,i] <- as.factor(df[,i]) 
+  }  
+  
+  vif_stats <- data.frame(
+    var = character(0),
+    vifvalue = numeric(0),
+    stringsAsFactors=F
+  )
+  
+  vif_hist <- data.frame(
+    var = character(0),
+    vifvalue = numeric(0),
+    stringsAsFactors=F
+  )
+  
+  max_vif <- vif_criteria+1
+  exclude <- c()
+  
+  while (max_vif>vif_criteria) {
+    vif_stats <- vif_stats[0,]
+    for (l in vars[!vars %in% exclude]) {
+      newvars <- vars[!vars %in% exclude]
+      tekst <- paste("mulcolmod <- lm(",l,"~.,data=df[,newvars])")
+      eval(parse(text=tekst))
+      r_square  <- summary(mulcolmod)$r.squared
+      vif <- ifelse(is.na(r_square),1000000000,ifelse(r_square==1,1000000000,1/(1-r_square)))
+      #print(paste(l,":",vif))
+      vif_stats[nrow(vif_stats)+1,c("var","vifvalue")] <- list(l,vif)
+    }
+    
+    vif_stats <- vif_stats[order(-vif_stats$vifvalue),]
+    
+    if (vif_stats[1,"vifvalue"]>vif_criteria) {
+      exclude <- c(exclude,vif_stats[1,"var"])
+      vif_hist[nrow(vif_hist)+1,c("var","vifvalue")] <- vif_stats[1,c("var","vifvalue")]
+    }
+    max_vif <- vif_stats[1,"vifvalue"]
+  }
+  
+  restvars <- vars[!vars %in% exclude]
+  
+  out <- list(restvars,vif_hist)
+  names(out) <- c("restvars","vif_hist")
+  return(out)
+}  
+
 
 inf.omit <- function(df) {
   for (i in colnames(df)) {
