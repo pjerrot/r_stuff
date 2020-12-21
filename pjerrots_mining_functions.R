4536f87171daf51c4cf8cc682483cef28024c5de
John Westberg <john.westberg@gmail.com>
2020-12-21 15:59:13 +0100

    Adding a detachAllPackages function

--- pjerrots_mining_functions.R
+++ pjerrots_mining_functions.R
@@ -1563,3 +1563,10 @@ clean_df_for_strange_characters <- function(df) {
   }
   return(df)
 }
+
+detachAllPackages <- function() {
+  basic.packages <- c("package:stats","package:graphics","package:grDevices","package:utils","package:datasets","package:methods","package:base")
+  package.list <- search()[ifelse(unlist(gregexpr("package:",search()))==1,TRUE,FALSE)]
+  package.list <- setdiff(package.list,basic.packages)
+  if (length(package.list)>0)  for (package in package.list) detach(package, character.only=TRUE)
+}
