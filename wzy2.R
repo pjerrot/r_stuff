72a2c538a27ec86b9c90eb4aa18c9f20b39d6170
John Westberg <john.westberg@gmail.com>
2022-08-16 23:12:08 +0200

    Updating barchart

--- wzy2.R
+++ wzy2.R
@@ -267,16 +267,16 @@ wz.wrapup <- function() {
 
 # insert.BARCHART ####
 wz.barchart <- function(df, x, group_var=NULL, num_vars, fun=c("asis","sum","mean","median","sd"), 
-                        annotation_var = NULL,
-                        chart_title=NULL,
-                        stacked = FALSE,
-                        fullstacked = FALSE,
-                        titlefontsize=18,
-                        #subtitle = NULL,
-                        legendposition = c("right"),
-                        align="center", width="600", height="600",
-                        chart_options = NULL,
-                        id = floor(runif(1,100,999))) {
+         annotation_var = NULL,
+         chart_title=NULL,
+         stacked = FALSE,
+         fullstacked = FALSE,
+         titlefontsize=18,
+         #subtitle = NULL,
+         legendposition = c("right"),
+         align="center", width="600", height="600",
+         chart_options = NULL,
+         id = floor(runif(1,100,999))) {
   library(dplyr)
   
   tilfstr <- as.character(id)
@@ -292,6 +292,34 @@ wz.barchart <- function(df, x, group_var=NULL, num_vars, fun=c("asis","sum","mea
   # prepping data
   if (fun=="asis") {
     df2 <- df
+    
+    
+    if (!is.null(group_var)) {
+      df2 <- dcast(df2,as.formula(paste0(x," ~ ",group_var)),value.var=num_vars[1])
+      # column names 
+      htmp <- paste0(htmp, paste0("['",x,"','",paste(colnames(df2)[-1],collapse="','"),"'",ifelse(!is.null(annotation_var),", { role: 'annotation' }",""),"],\n"))
+      
+      # data values
+      df2 <- data.frame(df2)
+      df2[is.na(df2)] <- 0
+      for (i in 1:nrow(df2)) {
+        htmp <- paste0(htmp, paste0("['",as.character(df2[i,x]),"',",as.character(paste(df2[i,colnames(df2)[-1]],collapse=",")),
+                                    ifelse(!is.null(annotation_var),paste0(",'",as.character(df2[i,annotation_var]),"'"),""),"]",ifelse(i==nrow(df2),"",","),"\n"))
+      }
+      
+    } else { # no group_var
+      # column names 
+      htmp <- paste0(htmp, paste0("['",x,"','",paste(num_vars,collapse="','"),"'",ifelse(!is.null(annotation_var),", { role: 'annotation' }",""),"],\n"))
+      
+      # data values
+      df2 <- data.frame(df2)
+      for (i in 1:nrow(df2)) {
+        htmp <- paste0(htmp, paste0("['",as.character(df2[i,x]),"',",as.character(paste(df2[i,num_vars],collapse=",")),
+                                    ifelse(!is.null(annotation_var),paste0(",'",as.character(df2[i,annotation_var]),"'"),""),"]",ifelse(i==nrow(df2),"",","),"\n"))
+      }
+    }
+    
+    
   } else {
     df2 <- df[!is.na(df[,num_vars]),]
     if (!is.null(group_var)) {
@@ -333,7 +361,7 @@ wz.barchart <- function(df, x, group_var=NULL, num_vars, fun=c("asis","sum","mea
   htmp <- paste0(htmp,"colors: [",.colorstr,"],\n")
   htmp <- paste0(htmp,"titleTextStyle: {fontSize: ",titlefontsize,"},\n")
   if (length(num_vars)==1) {htmp <- paste0(htmp,"hAxis: {title: '",gsub("asis ","",paste(fun,num_vars[1])),"'},\n")}
-  htmp <- paste0(htmp,"vAxis: {title: '",group_var,"'},\n")
+  htmp <- paste0(htmp,"vAxis: {title: '",x,"'},\n")
   htmp <- paste0(htmp,"bar: {groupWidth: '95%'},\n")
   htmp <- paste0(htmp,"legend: { position: '",legendposition,"' },\n")
   
@@ -360,8 +388,6 @@ wz.barchart <- function(df, x, group_var=NULL, num_vars, fun=c("asis","sum","mea
   .wjavafuns <<- c(.wjavafuns,htmp)
   .wcontent <<- c(.wcontent,paste0("<table align='",align,"' cellpadding=",.cellpadding,",><tr><td><div id='barchart_values",tilfstr,"'></div></td></tr></table>\n"))
 }
-
-
 
 # insert.ggplot ####
 wz.ggplot <- function(plot, chart_title=NULL, titlefontsize=18, width=NULL, height=NULL, align="center") {
